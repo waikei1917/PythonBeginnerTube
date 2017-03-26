f5305f40690c36fb3137864be9e36e477caa27b8
Hal <waikei1917oct@gmail.com>
2017-03-25 21:27:36 -0600

    Python

--- T55DictionaryKeySort.py
+++ T55DictionaryKeySort.py
@@ -25,3 +25,17 @@ print(30*'=')
 
 for x in sorted(users,key=itemgetter('fname','lname')):
     print(x)
+
+print(30*'=')
+
+a = [1,2,3]
+b = itemgetter(1)
+print(b(a))
+b = itemgetter(1,0)
+print(b(a))
+#要注意，operator.itemgetter函数获取的不是值，而是定义了一个函数，通过该函数作用到对象上才能获取值。
+
+print(30*'=')
+
+students = [('john','A',15),('jane','B',12),('dave','B',10)]
+print(sorted(students,key=lambda student:student[2]))
